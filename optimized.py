b679b59f33f3a113f6ceec5f6aefca3953f5cba2
Slb59 <sylvie.bricout2@laposte.net>
2023-03-23 15:44:31 +0100

    optimized algorithm

--- optimized.py
+++ optimized.py
@@ -1,3 +1,26 @@
+"""
+Nombre d'actions en entrée: 20
+Temps d'exécution: 0.0000s
+Nombre d'actions en sortie: 12
+-----------------
+Actions retenues:
+Action-10 prix: 34.0 € benefice: 9.18 €
+Action-6 prix: 80.0 € benefice: 20.0 €
+Action-13 prix: 38.0 € benefice: 8.74 €
+Action-19 prix: 24.0 € benefice: 5.04 €
+Action-4 prix: 70.0 € benefice: 14.0 €
+Action-20 prix: 114.0 € benefice: 20.52 €
+Action-5 prix: 60.0 € benefice: 10.2 €
+Action-11 prix: 42.0 € benefice: 7.14 €
+Action-18 prix: 10.0 € benefice: 1.4 €
+Action-17 prix: 4.0 € benefice: 0.48 €
+Action-16 prix: 8.0 € benefice: 0.64 €
+Action-14 prix: 14.0 € benefice: 0.14 €
+Résultats:
+Coût total: 498.0 €
+benefice total: 97.48 €
+"""
+
 import csv
 from dataclasses import dataclass
 from time import time
@@ -14,7 +37,7 @@ class Action:
     profit: float
 
     @property
-    def benefice(self) -> float:
+    def benefit(self) -> float:
         return self.price * self.profit / 100
     
 def load_dataset() -> list:
@@ -26,9 +49,9 @@ def load_dataset() -> list:
             result.append(action)
     return result
 
-def print_actions(dataset: list) -> None:
+def print_stocks(dataset: list) -> None:
     for action in dataset:
-        print(f"{action.name} prix: {action.price} € benefice: {action.benefice} €")
+        print(f"{action.name} prix: {action.price} € benefice: {action.benefit} €")
 
 
 def totalcost(dataset: list) -> float:
@@ -36,7 +59,7 @@ def totalcost(dataset: list) -> float:
 
 
 def totalbenef(dataset: list) -> float:
-    return sum(action.benefice for action in dataset)
+    return sum(action.benefit for action in dataset)
 
 
 def print_totalcost(dataset: list) -> None:
@@ -47,9 +70,15 @@ def print_totalbenef(dataset: list) -> None:
 
 def optimized(dataset: list) -> list:
     result = []
-    maxbenef = 0
-    n = len(dataset)
-    ...
+    stock_list_cost = 0
+    # sort dataset by reverse profit
+    sorted_dataset = sorted(dataset, key=lambda stock: stock.profit, reverse=True)
+    for stock in sorted_dataset:
+        if stock.price + stock_list_cost <= MAX_INVEST:
+            stock_list_cost += stock.price
+            result.append(stock)
+        if stock_list_cost == MAX_INVEST:
+            break
     return result
 
 if __name__ == "__main__":
@@ -62,7 +91,7 @@ if __name__ == "__main__":
     print(f"Nombre d'actions en sortie: {len(best_stocks)}")
     print("-----------------")
     print("Actions retenues:")
-    print_actions(best_stocks)
+    print_stocks(best_stocks)
     print("Résultats:")
     print_totalcost(best_stocks)
     print_totalbenef(best_stocks)
